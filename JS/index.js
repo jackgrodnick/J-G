40aa0beef834410f9b67b7a49b720d16e01ac3df
Jack Grodnick <jackgrodnick@jacks-mbp.lan>
2020-06-08 20:03:08 -0500

    idk more js

--- JS/index.js
+++ JS/index.js
@@ -251,3 +251,55 @@
   }
 
 </script>
+
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+let storyWords = story.split(' ');
+console.log('words: ', storyWords.length);
+
+let betterWords = storyWords.filter(val => {
+    if (unnecessaryWords.includes(val)) {
+      return;
+    } else {
+      return (val);
+    };
+});
+
+let over = {
+  'really': 0,
+  'very': 0,
+  'basically': 0,
+};
+
+storyWords.forEach((val) => {
+  if (overusedWords.includes(val)) {
+    over[val]++
+  } else {
+    return;
+  }
+});
+
+console.log('really: ', over.really);
+console.log('very: ', over.very);
+console.log('basically: ', over.basically);
+
+let sentences = {
+  sentencess: 0,
+};
+
+storyWords.forEach(val => {
+  let vall = val.length - 1;
+  if (val[vall] === ('.' || '!')) {
+    sentences.sentencess++;
+  } else {
+    return;
+  }
+});
+
+console.log('sentences: ', sentences.sentencess);
+
+console.log(betterWords.join(' '))
